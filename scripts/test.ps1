8be2da3b3e9dd0ee70fd3e33ad71e1572e70e2c8
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2018-11-11 16:48:10 +0800

    Update test runner so it actually fails a build in azure pipelines

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,2 +1,2 @@
 $testProjectPath = Join-Path -Path $PSScriptRoot -ChildPath "..\src\Belial.Tests\Belial.Tests.csproj"
-dotnet test $testProjectPath --logger:trx
+dotnet test $testProjectPath --logger "trx;LogFileName=testresults.trx"
