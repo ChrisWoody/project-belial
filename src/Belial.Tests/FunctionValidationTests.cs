63cf25558ee8221f38105c9cea96ad6083f4ffa2
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2018-11-19 20:01:34 +0800

    Adds some basic validation tests around the functions, requiring certain components to exist

---
+++ src/Belial.Tests/FunctionValidationTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.Azure.WebJobs;
+using Xunit;
+
+namespace Belial.Tests
+{
+    public class FunctionValidationTests
+    {
+        [Fact]
+        public void AllPublicStaticMethodsOnFunctionClassHaveValidFunctionNameAttribute()
+        {
+            var functions = typeof(Functions).GetMethods(BindingFlags.Public | BindingFlags.Static);
+
+            var resultsExpectToBeEmpty = functions.Select(x =>
+                {
+                    var functionNameAttribute = x.GetCustomAttribute<FunctionNameAttribute>();
+                    if (functionNameAttribute == null || string.IsNullOrWhiteSpace(functionNameAttribute.Name))
+                        return x.Name;
+
+                    return null;
+                })
+                .Where(x => x != null);
+
+            Assert.Empty(resultsExpectToBeEmpty);
+        }
+
+        [Fact]
+        public void AllFunctionsHaveATrigger()
+        {
+            var functions = typeof(Functions).GetMethods(BindingFlags.Public | BindingFlags.Static);
+
+            var resultsExpectToBeEmpty = functions.Select(x =>
+                {
+                    var parameters = x.GetParameters();
+
+                    if (parameters.Any(p =>
+                        p.GetCustomAttribute<HttpTriggerAttribute>() != null ||
+                        p.GetCustomAttribute<QueueTriggerAttribute>() != null))
+                        return null;
+
+                    return x.Name;
+                })
+                .Where(x => x != null);
+
+            Assert.Empty(resultsExpectToBeEmpty);
+        }
+
+        [Fact]
+        public void AllFunctionsHaveAnOutputType()
+        {
+            var functions = typeof(Functions).GetMethods(BindingFlags.Public | BindingFlags.Static);
+
+            var resultsExpectToBeEmpty = functions.Select(x =>
+                {
+                    var parameters = x.GetParameters();
+
+                    if (parameters.Any(p =>
+                        p.GetCustomAttribute<QueueAttribute>() != null ||
+                        p.GetCustomAttribute<TableAttribute>() != null))
+                        return null;
+
+                    return x.Name;
+                })
+                .Where(x => x != null);
+
+            Assert.Empty(resultsExpectToBeEmpty);
+        }
+    }
+}
