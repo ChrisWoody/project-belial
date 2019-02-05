af67ba12b404e4d721c998ce96b6d337c381892c
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2019-02-05 20:35:21 +0800

    Update UI to look nicer/behave better, fix failing test

--- src/Belial.Tests/RefreshImagesTests.cs
+++ src/Belial.Tests/RefreshImagesTests.cs
@@ -2,6 +2,7 @@
 using Belial.Common;
 using Belial.Tests.Core;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Belial.Tests
@@ -9,27 +10,28 @@ namespace Belial.Tests
     public class RefreshImagesTests
     {
         [Fact]
-        public async Task GivenAValidRequest_WhenFunctionIsCalled_ThenOkObjectIsReturned()
-        {
-            var request = TestHelper.CreateRequest("[\"url\"]");
-
-            var response = await Functions.RefreshImagesFunction(request, new TestLogger(), new TestAsyncCollector<DownloadImageQueueMessage>());
-
-            Assert.IsType<OkObjectResult>(response);
-        }
-
-        [Fact]
         public async Task GivenAValidRequest_WhenFunctionIsCalled_ThenDownloadImageQueueIsPopulated()
         {
             var queue = new TestAsyncCollector<DownloadImageQueueMessage>();
-            var request = TestHelper.CreateRequest("[\"https://images-na.ssl-images-amazon.com/images/I/816K5KxglLL.jpg\",\"https://images-na.ssl-images-amazon.com/images/I/8147jfELDHL.jpg\"]");
+            var request = TestHelper.CreateRequest(JsonConvert.SerializeObject(new RefreshImagesHttpMessage
+            {
+                DontDownloadIfExists = true,
+                ImageUrlsToDownload = new[]
+                {
+                    "https://images-na.ssl-images-amazon.com/images/I/816K5KxglLL.jpg",
+                    "https://images-na.ssl-images-amazon.com/images/I/8147jfELDHL.jpg"
+                }
+            }));
 
             var response = await Functions.RefreshImagesFunction(request, new TestLogger(), queue);
 
+            Assert.IsType<OkObjectResult>(response);
             Assert.Equal("https://images-na.ssl-images-amazon.com/images/I/816K5KxglLL.jpg", queue.QueuedItems[0].ImageUrl);
             Assert.Equal("srxdscrqY0AfJcMXFV44tw==.jpg", queue.QueuedItems[0].Filename);
+            Assert.True(queue.QueuedItems[0].DontDownloadIfExists);
             Assert.Equal("https://images-na.ssl-images-amazon.com/images/I/8147jfELDHL.jpg", queue.QueuedItems[1].ImageUrl);
             Assert.Equal("z8o4LOo1ux8nHFDBXTagoA==.jpg", queue.QueuedItems[1].Filename);
+            Assert.True(queue.QueuedItems[1].DontDownloadIfExists);
         }
     }
 }
